2ded063ac6b7c3e1b197e12bd60898b4c588a742
Minoru Nakano <minoru.nakano@gmail.com>
2021-10-20 18:14:39 -0400

    exploring the API

---
+++ sandbox/run.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const roolURL = 'http://api.openmetrolinx.com/OpenDataAPI/api/V1/';
+
+const serviceAtAGlance = 'ServiceataGlance/Trains/All';
+
+(async() => {
+    const res = await axios.get(`${roolURL}${serviceAtAGlance}?key=${process.env.KEY}`)
+    const trips = res.data.Trips.Trip;
+    console.log(trips.length);
+})();
